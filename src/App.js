5715e2da6ee81e95dc1d31070a8fabfeeb82d01c
candrepa1 <candrepa1@gmail.com>
2021-03-05 19:18:43 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 import './styles/main.css';
 import Filters from './components/Filters';
 import ExtraPoke from './components/ExtraPoke';
 import Encounters from './components/Encounters';
 import Login from './components/Login';
 import ProtectedRoute from './components/ProtectedRoute';
-import { ProvideAuth } from './components/provider/AuthProvider.js';
 import Logout from './components/Logout';
 import WelcomeMessage from './components/WelcomeMessage';
+import { useAuth } from './components/provider/AuthProvider.js';
 
 function App() {
+  const { user } = useAuth();
+  console.log(user);
+
   return (
-    <ProvideAuth>
       <Router>
         <div className="App p-6 font-mono">
           <div className="flex justify-between items-center mb-2">
@@ -30,12 +32,11 @@ function App() {
               <Filters />
             </ProtectedRoute>
             <Route path="/">
-              <Login />
+              {user.user ? <Redirect to="/pokedex"/> : <Login />}
             </Route>
           </Switch>
         </div>
       </Router>
-    </ProvideAuth>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import { useAuth } from './provider/AuthProvider.js';
-import { useHistory, Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
     const [pokeTrainer, setPokeTrainer] = useState('');
 
-    const { signIn, user } = useAuth();
+    const { signIn } = useAuth();
     const history = useHistory();
 
     const handleChange = (e) => {
@@ -19,7 +19,6 @@ const Login = () => {
 
     return(
             <div className="flex flex-col items-center justify-center h-screen">
-                {user.user ? <Redirect to="/pokedex"/> : <> 
                 <h1 className="mb-6 text-center text-2xl">BECOME A POKEMON TRAINER!</h1>
                 <form onSubmit={handleSubmit}>
                     <input className="border-2 border-black rounded-md p-2 mr-2 w-96 capitalize" placeholder="Your Pokemon Trainer Name" value={pokeTrainer} onChange={handleChange}/>
@@ -27,7 +26,6 @@ const Login = () => {
                         <button className="border-2 rounded-md p-2 border-black font-bold">Login</button>
                     </div>
                 </form>
-                </>}
             </div>
     );
 }
